919f07c9be903da90c7d341f800474fa1975dfb8
Wako <dulo.rodrigo@gmail.com>
2019-04-16 16:01:14 -0500

    Prepara estructura de proyecto para bibliotecas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { AcercaComponent } from './paginas/acerca/acerca.component';
+import { TerminosComponent } from './paginas/terminos/terminos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent
+  },
+  {
+    path: 'acerca',
+    component: AcercaComponent
+  },
+  {
+    path: 'terminos',
+    component: TerminosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
