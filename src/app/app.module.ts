9d0518727969cd8aa514efec4a005d09f0f31c62
Wako <dulo.rodrigo@gmail.com>
2019-04-16 17:26:07 -0500

    Crea componente elegir idioma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './componentes/footer/footer.component';
 import { InicioComponent } from './paginas/inicio/inicio.component';
 import { AcercaComponent } from './paginas/acerca/acerca.component';
 import { TerminosComponent } from './paginas/terminos/terminos.component';
+import { ElegirIdiomaComponent } from './componentes/elegir-idioma/elegir-idioma.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { TerminosComponent } from './paginas/terminos/terminos.component';
     FooterComponent,
     InicioComponent,
     AcercaComponent,
-    TerminosComponent
+    TerminosComponent,
+    ElegirIdiomaComponent
   ],
   imports: [
     BrowserModule,
